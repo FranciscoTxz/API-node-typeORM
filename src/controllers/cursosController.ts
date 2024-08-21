9f7070c8345a22513ae03f2d4e212f72dad1e51c
FranciscoTxz <angelftv2002@hotmail.com>
2024-08-21 17:51:16 -0600

    CRUD cursos and end

--- src/controllers/cursosController.ts
+++ src/controllers/cursosController.ts
@@ -1,69 +1,141 @@
 import { Request, Response } from "express";
+import { AppDataSource } from "../db/conexion";
+import { Curso } from "../models/cursoModel";
+import { Profesor } from "../models/profesoresModel";
+import { Estudiante } from "../models/estudiantesModel";
 
 class CursosController {
-    constructor(){
+    constructor() {
 
     }
 
-    consultar(req: Request, res: Response){
+    async consultar(req: Request, res: Response) {
         try {
-            res.json({ message: "Consultar" });
+            const data = await AppDataSource.manager.find(Curso, { relations: ['profesor', 'estudiantes'] });
+            if (data.length === 0) {
+                res.status(404).json({ message: "No hay cursos" });
+            }
+            else {
+                res.status(200).json({ OK: data });
+            }
         } catch (err) {
-            if (err instanceof Error){
-                res.status(500).send(err.message); 
+            if (err instanceof Error) {
+                res.status(500).send(err.message);
             }
         }
     }
 
-    consultarDetalle(req: Request, res: Response){
+    async consultarDetalle(req: Request, res: Response) {
         try {
-            res.json({ message: "Consultar detalle" });
+            const { id } = req.params;
+            const registro = await AppDataSource.manager.findOne(Curso, { where: { id: Number(id) }, relations: ['profesor', 'estudiantes']  });
+            if (registro) {
+                res.status(200).json({ OK: registro });
+            }
+            else {
+                res.status(404).json({ message: "Curso no encontrado" });
+            }
         } catch (err) {
-            if (err instanceof Error){
-                res.status(500).send(err.message); 
+            if (err instanceof Error) {
+                res.status(500).send(err.message);
             }
         }
     }
 
-    ingresar(req: Request, res: Response){
+    async ingresar(req: Request, res: Response) {
         try {
-            res.json({ message: "Ingresar" });
+            const validar = await AppDataSource.manager.findOne(Profesor, { where: { id: req.body.profesor } });
+            if (!validar) {
+                res.status(404).json({ message: "Profesor no encontrado" });
+                return;
+            }
+            else {
+                const registro = await AppDataSource.manager.save(Curso, req.body);
+                if (registro) {
+                    res.status(201).json({ OK: registro });
+                }
+                else {
+                    res.status(404).json({ Error: "Curso no ingresado" });
+                }
+            }
         } catch (err) {
-            if (err instanceof Error){
-                res.status(500).send(err.message); 
+            if (err instanceof Error) {
+                res.status(500).send(err.message);
             }
         }
     }
 
-    actualizar(req: Request, res: Response){
-        const {id} = req.params;
+    async actualizar(req: Request, res: Response) {
         try {
-            res.json({ message: "Actualizar" });
+            const { id } = req.params;
+            const validar = await AppDataSource.manager.findOne(Profesor, { where: { id: req.body.profesor } });
+            if (!validar) {
+                res.status(404).json({ message: "Profesor no encontrado" });
+                return;
+            }
+            else {
+                const registro = await AppDataSource.manager.update(Curso, id, req.body);
+                if (registro.affected != 0) {
+                    res.status(200).json({ OK: "Curso actualizado" });
+                }
+                else {
+                    res.status(404).json({ message: "Curso no encontrado" });
+                }
+            }
         } catch (err) {
-            if (err instanceof Error){
-                res.status(500).send(err.message); 
+            if (err instanceof Error) {
+                res.status(500).send(err.message);
             }
         }
     }
 
-    borrar(req: Request, res: Response){
-        const {id} = req.params;
+    async borrar(req: Request, res: Response) {
         try {
-            res.json({ message: "Borrar" });
+            const { id } = req.params;
+            const registro = await AppDataSource.manager.delete(Curso, id);
+            if (registro.affected != 0) {
+                res.status(200).json({ OK: "Curso eliminado" });
+            }
+            else {
+                res.status(404).json({ message: "Curso no encontrado" });
+            }
         } catch (err) {
-            if (err instanceof Error){
-                res.status(500).send(err.message); 
+            if (err instanceof Error) {
+                res.status(500).send(err.message);
             }
         }
     }
 
-    asociarEstudiante(req: Request, res: Response){
-        const {id} = req.params;
+    async asociarEstudiante(req: Request, res: Response) {
         try {
-            res.json({ message: "asociarEstudiante" });
+            const estudiante = await AppDataSource.manager.findOne(Estudiante, { where: { id: req.body.estudiante_id } });
+            const curso = await AppDataSource.manager.findOne(Curso, { where: { id: req.body.curso_id } });
+            if (!estudiante) {
+                res.status(404).json({ message: "Estudiante no encontrado" });
+                return;
+            }
+            if (!curso) {
+                res.status(404).json({ message: "Curso no encontrado" });
+                return;
+            }
+
+            console.log('llega');
+            
+            curso.estudiantes = curso.estudiantes || [];
+            curso.estudiantes.push(estudiante);
+
+            const registro = await AppDataSource.manager.save(curso);
+
+            if (registro) {
+                res.status(200).json({ OK: "Estudiante asociado al curso" });
+            }
+            else {
+                res.status(400).json({ message: "Estudiante no asociado al curso" });
+            }
+
         } catch (err) {
-            if (err instanceof Error){
-                res.status(500).send(err.message); 
+            if (err instanceof Error) {
+                res.status(500).send(err.message);
             }
         }
     }
