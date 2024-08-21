426614840c72566ecaa889727266d6eb39cdbae5
FranciscoTxz <angelftv2002@hotmail.com>
2024-08-21 07:27:46 -0600

    CRUD Profesores

--- src/controllers/profesoresControllers.ts
+++ src/controllers/profesoresControllers.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from "express";
+import { AppDataSource } from "../db/conexion";
+import { Profesor } from "../models/profesoresModel";
 
 class ProfesoresController {
     constructor(){
 
     }
 
-    consultar(req: Request, res: Response){
+    async consultar(req: Request, res: Response){
         try {
-            res.json({ message: "Consultar" });
+            const data = await AppDataSource.manager.find(Profesor);
+            if (data.length === 0){
+                res.status(404).json({ message: "No hay registros" });
+            }
+            else {
+                res.status(200).json({OK: data});
+            }
         } catch (err) {
             if (err instanceof Error){
                 res.status(500).send(err.message); 
@@ -15,9 +23,16 @@ class ProfesoresController {
         }
     }
 
-    consultarDetalle(req: Request, res: Response){
+    async consultarDetalle(req: Request, res: Response){
         try {
-            res.json({ message: "Consultar detalle" });
+            const {id} = req.params;
+            const registro = await AppDataSource.manager.findOne(Profesor, { where: { id: Number(id) } });
+            if (registro){
+                res.status(200).json({OK: registro});
+            }
+            else {
+                res.status(404).json({ message: "Profesor no encontrado" });
+            }
         } catch (err) {
             if (err instanceof Error){
                 res.status(500).send(err.message); 
@@ -25,9 +40,15 @@ class ProfesoresController {
         }
     }
 
-    ingresar(req: Request, res: Response){
+    async ingresar(req: Request, res: Response){
         try {
-            res.json({ message: "Ingresar" });
+            const registro = await AppDataSource.manager.save(Profesor, req.body);
+            if (registro){
+                res.status(201).json({OK: registro});
+            }
+            else {
+                res.status(404).json({ Error: "Profesor no ingresado" });
+            }
         } catch (err) {
             if (err instanceof Error){
                 res.status(500).send(err.message); 
@@ -35,10 +56,17 @@ class ProfesoresController {
         }
     }
 
-    actualizar(req: Request, res: Response){
+    async actualizar(req: Request, res: Response){
         const {id} = req.params;
         try {
-            res.json({ message: "Actualizar" });
+            const registro = await AppDataSource.manager.findOne(Profesor, { where: { id: Number(id) } });
+            if (registro){
+                await AppDataSource.manager.update(Profesor, id, req.body);
+                res.status(200).json({OK: "Profesor actualizado"});
+            }
+            else {
+                res.status(404).json({ message: "Profesor no encontrado" });
+            }
         } catch (err) {
             if (err instanceof Error){
                 res.status(500).send(err.message); 
@@ -46,10 +74,17 @@ class ProfesoresController {
         }
     }
 
-    borrar(req: Request, res: Response){
-        const {id} = req.params;
+    async borrar(req: Request, res: Response){
         try {
-            res.json({ message: "Borrar" });
+            const {id} = req.params;
+            const registro = await AppDataSource.manager.findOne(Profesor, { where: { id: Number(id) } });
+            if (registro){
+                await AppDataSource.manager.delete(Profesor, id);
+                res.status(200).json({OK: "Profesor eliminado"});
+            }
+            else {
+                res.status(404).json({ message: "Profesor no encontrado" });
+            }
         } catch (err) {
             if (err instanceof Error){
                 res.status(500).send(err.message); 
