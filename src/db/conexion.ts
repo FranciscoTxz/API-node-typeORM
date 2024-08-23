e1c8b2bc01576c259853e76712b11dcad93168ab
FranciscoTxz <angelftv2002@hotmail.com>
2024-08-23 07:46:08 -0600

    Extra conecction

--- src/db/conexion.ts
+++ src/db/conexion.ts
@@ -3,13 +3,25 @@ import { Estudiante } from "../models/estudiantesModel";
 import { Profesor } from "../models/profesoresModel";
 import { Curso } from "../models/cursoModel";
 
-export const AppDataSource = new DataSource({
+/* export const AppDataSource = new DataSource({
     type: 'mysql',
     host: 'MacBook-Air-de-Angel-2.local',
     port: 3306,
     username: 'root',
     password: 'Pato1234',
     database: 'cursos2',
+    logging: true,
+    entities: [Estudiante, Profesor, Curso],
+    //synchronize: true
+}); */
+
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: 'junction.proxy.rlwy.net',
+    port: 40260,
+    username: 'root',
+    password: 'AXDBtOdwdjXUQnNnLRWNVtYboEcRCsNH',
+    database: 'railway',
     logging: true,
     entities: [Estudiante, Profesor, Curso],
     //synchronize: true
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import app from "./app";
 import { AppDataSource } from "./db/conexion";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
 
 async function main(){
     try {
         await AppDataSource.initialize();
         console.log("Database is connected");
-        app.listen(3500, () => {
-            console.log("Server is running on http://localhost:3500");
+        app.listen(port, () => {
+            console.log("Server is running on port:" + port);
         });
     } catch (error) {
         if (error instanceof Error) {
             console.error(error.message);
         }
     }
-    
 }
 
 main();
